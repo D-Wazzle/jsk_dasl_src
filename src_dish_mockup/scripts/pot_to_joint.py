0d2121637ceafd3a5eb7b1755e690bb5ef4f276e
yoshimalucky <y-tanaka@jsk.imi.i.u-tokyo.ac.jp>
2017-05-25 09:18:01 +0900

    added src_hubo package.

--- src_dish_mockup/scripts/pot_to_joint.py
+++ src_dish_mockup/scripts/pot_to_joint.py
@@ -9,11 +9,11 @@ pub_1 = rospy.Publisher('pan_controller/command', Float64, queue_size=10)
 pub_2 = rospy.Publisher('tilt_controller/command', Float64, queue_size=10)
 
 def cb1(data):
-    data.data = 0.75 * (2 * data.data / 1024.0 - 1)
+    data.data = 0.75 * (2 * data.data / 1024.0 - 1) * 2 # 1024
     pub_1.publish(data)
 
 def cb2(data):
-    data.data = 0.75 * (2 * data.data / 1024.0 - 1)
+    data.data = 0.75 * (2 * data.data / 1024.0 - 1) * 2 # 1024
     pub_2.publish(data)
 
 # def pot_sub():
